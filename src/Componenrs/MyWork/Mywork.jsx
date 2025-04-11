0e2cd0da421223d3fc3214c77166041bf43f31cd
SubhadeepGarai <Subhadeepgarai2@gmail.com>
2025-04-11 15:42:01 +0530

    edit in page responsiveness

--- src/Componenrs/MyWork/Mywork.jsx
+++ src/Componenrs/MyWork/Mywork.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MyWork.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import arrow_icon from '../../assets/arrow_icon.svg'
 import mywork_data from '../../assets/mywork_data'
-// import arrow_icon from '../../assets/arrow_icon.svg'
-
 
 const Mywork = () => {
+  const [showAll, setShowAll] = useState(false);
+  const itemsPerRow = 3; // 3 columns
+  const initialRows = 2; // Show 2 rows initially
+  const initialItems = itemsPerRow * initialRows;
+  
+  const displayedItems = showAll ? mywork_data : mywork_data.slice(0, initialItems);
+
+  const handleShowMore = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div id='work' className='mywork'>
       <div className="mywork-title">
@@ -13,15 +23,18 @@ const Mywork = () => {
         <img src={theme_pattern} alt=""/>
       </div>
       <div className="mywork-container">
-      {mywork_data.map((work,index)=>{
-        return <img key={index} src={work.w_img} alt=""/>
-        
-      })}
+        {displayedItems.map((work, index) => (
+          <a key={index} href={work.W_link}>
+            <img src={work.w_img} alt=""/>
+          </a>
+        ))}
       </div>
-      {/* <div className="mywork-showmore">
-        <p>Show More</p>
-        <img src={arrow_icon} alt=""/>
-      </div> */}
+      {mywork_data.length > initialItems && (
+        <div className="mywork-showmore" onClick={handleShowMore}>
+          <p>{showAll ? 'Show Less' : 'Show More'}</p>
+          <img src={arrow_icon} alt="" style={{ transform: showAll ? 'rotate(180deg)' : 'none' }}/>
+        </div>
+      )}
     </div>
   )
 }
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,39 +3,55 @@ import project2_img from '../assets/project2.png'
 import project3_img from '../assets/project3.png'
 import project4_img from '../assets/project4.png'
 import project5_img from '../assets/project5.png'
-import project6_img from '../assets/project_6.svg'
+import project6_img from '../assets/project6.png'
+import project7_img from '../assets/project7.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        W_link:"https://github.com/SubhadeepGarai/Portfolio"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        W_link:"https://github.com/SubhadeepGarai/Gemini-Clone"
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        W_link:"https://github.com/SubhadeepGarai/secure_bank_app"
+
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        W_link:"https://github.com/SubhadeepGarai/Netflix-clone"
     },
     {
         w_no:5,
         w_name:"hardware project",
-        w_img:project5_img
+        w_img:project5_img,
+        W_link:"https://github.com/SubhadeepGarai/Spotify-Clone"
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        W_link:"https://github.com/SubhadeepGarai/food-ordering-site"
+    },
+    {
+        w_no:7,
+        w_name:"Hardware",
+        w_img:project7_img,
+        W_link:"https://docs.google.com/presentation/d/1sqj-YtSLivOaz8qlPxmoAYC94cBKsCoh/edit?usp=sharing&ouid=112937513587925900522&rtpof=true&sd=true"
     },
 ]
  
 export default mywork_data;
+
+
